d3a2860db34754177b3e7b407aa55fd5cd066215
Gilbor Camporazo Jr. <gilborcamporazojr@programmer.net>
2025-04-10 23:59:11 +0800

    Modified review counter to get and count valid reviews submitted.

--- scripts/review.js
+++ scripts/review.js
@@ -1,14 +1,38 @@
 const urlParams = new URLSearchParams(window.location.search);
-const productName = urlParams.get("product-name");
 
-if (productName) {
-    let count = parseInt(localStorage.getItem('review-counter'));
-    if (isNaN(count)) {
-        count = 0;
-    }
-    count++;
-    localStorage.setItem('review-counter', count);
-    const reviewCounter = document.getElementById("review-counter");
-    reviewCounter.textContent = count;
+// Form fields (Required)
+const productName = urlParams.get("product-name");
+const overallRating = urlParams.get("overall-rating");
+const dateOfInstallation = urlParams.get("date-of-installation");
+
+// Form fields (Optional)
+const durability = urlParams.get("durability");
+const easeOfUse = urlParams.get("ease-of-use");
+const performance = urlParams.get("performance");
+const design = urlParams.get("design");
+const review = urlParams.get("review");
+const name = urlParams.get("name");
+
+const reviews = JSON.parse(localStorage.getItem('reviews')) || [];
+
+let reviewsCount = reviews.length;
+
+if (productName && overallRating && dateOfInstallation) {
+    const reviewObject = {
+        productName: productName,
+        overallRating: overallRating,
+        dateOfInstallation: dateOfInstallation,
+        durability: durability || null,
+        easeOfUse: easeOfUse || null,
+        performance: performance || null,
+        design: design || null,
+        review: review || null,
+        name: name || null,
+    };
+    reviews.push(reviewObject);
+    localStorage.setItem('reviews', JSON.stringify(reviews));
+    reviewsCount = reviews.length;
 }
 
+const reviewCounter = document.getElementById("review-counter");
+reviewCounter.textContent = reviewsCount;
